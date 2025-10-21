915529e3f390fe2d615bb522ac036261a96f296f
DavyL <leo.davy@ens-lyon.fr>
2025-10-21 17:20:51 +0200

    Starting to include stuff for heterogenous texture analysis

---
+++ src/lir3a/AnalyzeTextures.py
@@ -0,0 +1,18 @@
+from dtcwt.utils import compute_wavelet_coefs
+from LinRegPhysics import LinRegPhys
+
+def compute_dtcwt(X):
+    coefs = compute_wavelet_coefs(X)
+    return coefs
+
+def compute_features_from_coefs(C):
+    return 
+
+def compute_global_features_from_coefs(C):
+    return 
+
+def compute_global_pw_features_coefs(C):
+    return 
+
+def compute_log_coefs(X):
+    return
